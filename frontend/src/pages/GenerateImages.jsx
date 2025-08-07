ff0d5abb9e7b578020766ddaed3702409dfde7fa
shubham sagsr <shubhamwesley143@gmsail.com>
2025-08-08 01:10:09 +0530

    "updated GeneratedImages"

--- frontend/src/pages/GenerateImages.jsx
+++ frontend/src/pages/GenerateImages.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 const GenerateImages = () => {
   const  imageStyle = ['Realistic', 'Ghibli style', 'Anime style', 'Cartoon style', 'Fantasy style', 'Realistic style', '3D style', 'Portrait style']
   const [selectedStyle, setSelectedStyle] = useState('Realistic')
-  const [input, setInput] = useState()
+  const [input, setInput] = useState('')
 const [publish, setPublish] = useState(false)
   const onSubmitHandler=async (e) => {
     e.preventDefault()
@@ -31,8 +31,8 @@ const [publish, setPublish] = useState(false)
 'text-gray-500 border-gray-300'}`} key={item}>{item}</span>
   ))}
 </div>
-<div class= "my-6 flex items-center gap-2">
-  <label class= "relative cursor-pointer">
+<div className= "my-6 flex items-center gap-2">
+  <label className= "relative cursor-pointer">
     <input type= "checkbox" onChange= {(e) => setPublish(e.target.checked)} checked= {publish} className= 'sr-only peer' />
     <div className= "w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition"></div>
     <span className= "absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4"></span>
@@ -51,7 +51,7 @@ const [publish, setPublish] = useState(false)
   </div>
   <div className='flex-1 flex justify-center items-center'>
     <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-      <image className='w-9 h-9' />
+      <Image className='w-9 h-9' />
       <p>Enter a topic and click “Generate image” to get started</p>
     </div>
   </div>
