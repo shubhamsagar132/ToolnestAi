791c8e588509fa7e6fa29bad938a2bd10922a8f6
shubham sagsr <shubhamwesley143@gmsail.com>
2025-08-09 17:41:17 +0530

    "created routes Middlewares"

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import{neon} from "@neondatabase/serverless";
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 const app=express();
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 
 app.get("/", (req, res) => 
   res.send("Server is Live!")
 );
+app.use(requireAuth());
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
