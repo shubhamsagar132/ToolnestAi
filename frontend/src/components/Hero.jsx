a55d0a4386eebee8628d27aea84848170ef52874
shubham sagsr <shubhamwesley143@gmsail.com>
2025-08-19 23:57:18 +0530

    "updated hero.jsx"

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -13,7 +13,7 @@ return(
       min-h-screen
     "
   >
-  <div className='text-center mb-6'>
+  <div className='text-center mb-6'>;
     <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content<br/> with
     <span className='text-primary'></span> AI tools </h1>
 <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-x1 mx-auto max-sm:text-xs text-gray-600'>Transform your content creation with our
